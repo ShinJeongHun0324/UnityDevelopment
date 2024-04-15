2bcff5963f2a0a568cf941aaaf50690ae6730e97
minwn3318 <minwn3318@naver.com>
2024-04-15 21:09:02 +0900

    0415 20223413정민주
    작업내용
    - 타이머 스크립트 제작 -> 게임오버와 관련이 있어 게임매니저라고 지음
    - 게임 레벨 로그 찍기 스폰 매니저에 기입 -> 해당 파일을 복제해서 사용
    - 게임 진행시간 로그 찍기 -> 타이머 스크립트에 제작
    - 플레이어 스크립트 복제 후 수정 -> 파워업표시의 y값도 플레이어를 따라가는 것으로 변경

--- NewProject/Assets/Scripts/SpawnManager_20223413.cs
+++ NewProject/Assets/Scripts/SpawnManager_20223413.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager_20223413 : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public int enemyCount;
     public int waveNumber = 1;
     public GameObject powerupPrefab;
+    public int powerCount = 1;
 
     private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
+        // 웨이브 레벨 로그 찍기 - 정민주 20223413
+        Debug.Log("Level : " + waveNumber);
         SpawnEnemyWave(waveNumber);
 
-        Instantiate(powerupPrefab, GenerateSpawnPosition(),
-        powerupPrefab.transform.rotation);
+        SpawnPowerup(powerCount);
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -39,6 +41,16 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+    // 들어온 수만큼 파워업 생성함 - 정민주 20223413 
+    void SpawnPowerup(int powerupToSpanw)
+    {
+        for (int i = 0; i < powerupToSpanw; i++)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(),
+                powerupPrefab.transform.rotation);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -51,10 +63,18 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount == 0)
         {
             waveNumber++;
+            Debug.Log("Level : " + waveNumber);
             SpawnEnemyWave(waveNumber);
-
-            Instantiate(powerupPrefab, GenerateSpawnPosition(),
-                powerupPrefab.transform.rotation);
+            //웨이브레벨이 3의 배수일때마다 파워업 생성 갯수를 1늘림 - 정민주 20223413
+            if(waveNumber % 3 == 0)
+            {
+                powerCount++;
+                SpawnPowerup(powerCount);
+            }
+            else
+            {
+                SpawnPowerup(powerCount);
+            }
         }
     }
 }
