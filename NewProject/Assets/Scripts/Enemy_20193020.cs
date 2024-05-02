30ad7287ac1a923b89cb24059c5b30722acbeeba
ShinJeongHun <boknam818@gmail.com>
2024-05-02 17:47:36 +0900

    UI 게임시작시 꺼짐 완료
    작업내용
    - 게임오버변수 추가
    - 게임시작시 UI 비활성화
    - 게임 시작전에는 없던 오브잭트 생성

---
+++ NewProject/Assets/Scripts/Enemy_20193020.cs
@@ -0,0 +1,32 @@
+// using JetBrains.Rider.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_20193020 : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ NewProject/Assets/Scripts/PlayerController_20193020.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_20193020 : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
+
+    public GameObject powerupIndicator;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        focalPoint = GameObject.Find("Focal Point");
+
+        powerupIndicator = GameObject.FindWithTag("Indicator");
+        powerupIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward *
+                forwardInput * speed);
+
+        Vector3 piPosition = transform.position;
+        piPosition.y = -0.5f;
+        powerupIndicator.transform.position = piPosition;
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+
+        powerupIndicator.SetActive(false);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+
+        powerupIndicator.SetActive(true);
+
+        StartCoroutine(PowerupCountdownRoutine());
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody =
+            collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.transform.position -
+                            transform.position).normalized;
+
+            Debug.Log("Collided with " + collision.gameObject.name +
+                " with powerup set to " + hasPowerup);
+
+            enemyRigidbody.AddForce(awayFromPlayer *
+                        powerupStrength, ForceMode.Impulse);
+        }
+    }
+}
